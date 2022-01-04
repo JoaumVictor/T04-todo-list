f9efca07bb1767d17859f31e0892d777660f8bf1
João Victor <joaumvictor.oficial@gmail.com>
2022-01-03 23:01:38 -0300

    doideira na sala 4

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let botaoDeTarefa = document.getElementById("criar-tarefa");
-let textoDeEntrada = document.getElementById("texto-tarefa");
-let lista = document.getElementById("lista-tarefas");
+const botaoDeTarefa = document.getElementById("criar-tarefa");
+const textoDeEntrada = document.getElementById("texto-tarefa");
+const lista = document.getElementById("lista-tarefas");
+const alvo = document.getElementsByClassName("item");
 
 botaoDeTarefa.addEventListener("click", addItem)
 
@@ -9,14 +10,24 @@ function addItem() {
     let itemDaLista = document.createElement("li");
     itemDaLista.className = "item";
     itemDaLista.innerHTML = textoDeEntrada.value;
+    itemDaLista.addEventListener("click", alteraCor)
+    itemDaLista.addEventListener("dblclick", riscadinho)
     textoDeEntrada.value = "";
     lista.appendChild(itemDaLista);
 }
 
 
-function alteraCor() {
-itemAlvo.style.backgroundcolor = "rgb(128,128,128)";
+function alteraCor(event) {
+    for(i=0; i < alvo.length; i+=1) {
+    alvo[i].style.backgroundColor = "";
+    }
+    event.target.style.backgroundColor = "rgb(128, 128, 128)";
 }
+
+function riscadinho(event) {
+event.target.classList.toggle("completed")
+}
+
 
 
 
